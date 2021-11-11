ce1cdb5268718bb407d28d5b68acb7585768a78b
saadullah123-cyber <suashraf28@gmail.com>
2021-11-11 23:24:19 +0500

    Add project files.

---
+++ Twitter/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Twitter.Models;
+
+namespace Twitter.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationDbContext context;
+        private readonly ILogger<HomeController> _logger;
+
+
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            this.context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var post =await context.post.OrderByDescending(x=>x.ID).ToListAsync();
+            return View(post);
+        }
+        [HttpPost]
+        public async Task<JsonResult> Save(string message)
+        {
+            try
+            {
+                post post = new post();
+                post.Message = message;
+               
+                context.post.Add(post);
+                await context.SaveChangesAsync();
+                return Json("OK");
+            }
+            catch (Exception ex)
+            {
+                return Json("Error");
+            }
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Twitter/Migrations/20211111181026_init.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Twitter.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "post",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Message = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_post", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "post");
+        }
+    }
+}
